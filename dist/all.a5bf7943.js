10605b3ae3fb513c98cdd399305247010de1c5d6
shahfahadmansoori <shahfahadmansoori345@gmail.com>
2019-12-30 12:22:26 +0500

    gutter-images-btn-updated

--- dist/all.a5bf7943.js
+++ dist/all.a5bf7943.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56113" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51141" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
